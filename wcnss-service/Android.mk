cf7974ed225cdde00c2203e4db7a24e625091cad
Ethan Chen <intervigil@gmail.com>
2014-11-13 11:27:34 -0800

    wcnss_qmi: Allow targets to provide custom libwcnss_qmi
    Change-Id: I9aedd4b8633a81ecb3cd292299826e3daca60656
    Signed-off-by: theh2o64@gmail.com <theh2o64@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -11,9 +11,15 @@ LOCAL_SRC_FILES := wcnss_service.c
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
 
 ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
+
+ifeq ($(TARGET_PROVIDES_WCNSS_QMI),true)
+LOCAL_CFLAGS += -DWCNSS_QMI_OSS
+LOCAL_SHARED_LIBRARIES += libdl
+else
 ifeq ($(TARGET_USES_WCNSS_MAC_ADDR_REV),true)
 LOCAL_CFLAGS += -DWCNSS_QMI_MAC_ADDR_REV
 endif
+
 ifneq ($(QCPATH),)
 LOCAL_CFLAGS += -DWCNSS_QMI
 LOCAL_SHARED_LIBRARIES += libwcnss_qmi
@@ -21,6 +27,9 @@ else
 LOCAL_CFLAGS += -DWCNSS_QMI_OSS
 LOCAL_SHARED_LIBRARIES += libdl
 endif #QCPATH
+
+endif #TARGET_PROVIDES_WCNSS_QMI
+
 endif #TARGET_USES_QCOM_WCNSS_QMI
 
 LOCAL_MODULE_TAGS := optional
@@ -28,6 +37,7 @@ LOCAL_CFLAGS += -Wall -Werror
 
 include $(BUILD_EXECUTABLE)
 
+ifneq ($(TARGET_PROVIDES_WCNSS_QMI),true)
 ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
 ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
@@ -57,6 +67,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 endif #QCPATH
 endif #TARGET_USES_QCOM_WCNSS_QMI
+endif #TARGET_PROVIDES_WCNSS_QMI
 
 endif #TARGET_ARCH == arm
 endif
